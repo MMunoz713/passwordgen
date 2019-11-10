cf5bcf0d1817b215ade0fe6da9a094693f17d960
smarties713 <odst.mrtn@gmail.com>
2019-11-09 23:25:09 -0600

    added copy element to copy button

--- script.js
+++ script.js
@@ -18,11 +18,18 @@ function generate(){
 document.getElementById("length").innerHTML = "Length: 25";
 
 document.getElementById("slider").oninput = function(){
-    
+
     if (document.getElementById("slider").value > 0){
         document.getElementById("length").innerHTML = "length: " + document.getElementById("slider").value;
     }
     else{
         document.getElementById("length").innerHTML = "length: 1";
     }
+
+}
+
+function copyPassword(){
+    document.getElementById("display").select();
+    document.execCommand("Copy");
+    alert("Password has been copied to clipboard")
 }
