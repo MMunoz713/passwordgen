93ad2c47c3eac7e1613d57a12ae97464c24255b8
smarties713 <odst.mrtn@gmail.com>
2019-11-09 19:05:37 -0600

    added characters used for generating password

---
+++ script.js
@@ -0,0 +1,9 @@
+function generate(){
+
+    let complexity = document.getElementById("slider").value;
+
+    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+~`";
+
+    let password = ""
+
+}
