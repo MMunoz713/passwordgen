06834d52029a35f2d4762595dd9d8c4767d6d0b6
smarties713 <odst.mrtn@gmail.com>
2019-11-09 22:39:59 -0600

    updated js file to generate password

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
+//generate random password
 function generate(){
 
     let complexity = document.getElementById("slider").value;
 
     let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+~`";
 
-    let password = ""
+    let password = "";
+
+    for(var i = 0; i <= complexity; i++){
+        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length -1)));
+    }
+
+    document.getElementById("display").value = password;
 
 }
